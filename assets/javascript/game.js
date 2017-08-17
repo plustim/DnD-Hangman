d5a1cd495ec274e0779754154ec99aa72062548d
plustim <tim@plustim.com>
2017-08-17 00:47:36 -0700

    working logic now

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,37 @@
 var hangman = {
 	wins : 0,
-	guessesRemaining : 200,
+	guessesRemaining : 0,
 	wrongLetters : [],
-	currentWord : "COWBOY",
+	currentWord : "",
+	currentGuess : "",
 	wordList : ["CAPITOLS", "LETTERS"],
 	handler : function(letter){
 		// input must be a letter not guessed
 		if( "ABCDEFGHIJKLMNOPQRSTUVWXYZ".includes(letter) && !hangman.wrongLetters.includes(letter) ){
 			// determine if guessed letter is correct
 			this.checkLetter( letter );
-			
-			// if solved, update score and reset board
 		}		
 	},
 	checkLetter : function( letter ){
 		if( this.currentWord.includes(letter) ){
 			// insert all instances of this letter in word
-			// 
-			this.updateStatus();
-			this.checkWord();
+ 			var guessUpdate = "";
+ 			for( var i=0; i<this.currentWord.length; i++){
+				if( this.currentWord.charAt(i) === letter ){
+					document.getElementById("letter"+i).innerHTML = letter;
+					guessUpdate += letter;
+				}else if( this.currentGuess.charAt(i) ){
+					guessUpdate += this.currentGuess.charAt(i);
+				}else{
+					guessUpdate += 0;
+				}
+			}
+			this.currentGuess = guessUpdate;
+			console.log(this.currentGuess);
+			if( this.currentWord === this.currentGuess ){
+				this.wins++;
+				this.newWord();
+			}
 		}else{ // word does not include given letter
 			this.wrongLetters.push(letter);
 			var wrongLettersString = this.wrongLetters[0];
@@ -28,19 +41,30 @@ var hangman = {
 				}
 			}
 			document.getElementById("guesses").innerHTML = wrongLettersString;
-			this.updateStatus();
+			this.guessesRemaining--;
+			document.getElementById("remaining").innerHTML = this.guessesRemaining + " guesses remaining.";
+			if(!this.guessesRemaining){
+				alert("Out of guesses! You won " + this.wins +" times.");
+				this.wins = 0;
+				this.newWord();
+			}
 		}
 	},
-	// update correct and incorrect guesses and display
-	updateStatus : function(){
-
-	},
-	// check if word is solved
-	checkWord : function(){
-
+	newWord : function(){
+		this.currentWord = this.wordList[Math.floor(Math.random()*this.wordList.length)];
+		this.guessesRemaining = 12;
+		document.getElementById("remaining").innerHTML = this.guessesRemaining + " guesses.";
+		var wordString = "";
+		for( var i=0; i<this.currentWord.length; i++ ){
+			wordString += "<div id='letter"+i+"'>&nbsp;</div>";
+		}
+		document.getElementById("word").innerHTML = wordString;
+		document.getElementById("guesses").innerHTML = "";
 	}
 };
 
+// start a new hangman
+hangman.newWord();
 
 // user enters keypress letter
 document.onkeyup = function(event) {
